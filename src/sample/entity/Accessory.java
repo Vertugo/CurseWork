b120179b080e4a7a30700b59be0aa3649ba75248
Vertugo <mmoolldd@mail.ru>
2016-11-16 23:13:38 +0200

    Add entity package

---
+++ src/sample/entity/Accessory.java
@@ -0,0 +1,12 @@
+package sample.entity;
+
+/**
+ * Created by Alex on 16.11.2016.
+ */
+public abstract class Accessory {
+
+    protected String name;
+    protected Double price;
+    protected Integer guarantee;
+    
+}
